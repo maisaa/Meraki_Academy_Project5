8ca8a63461899ed303300020432e66e7e3d9658f
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-06-23 21:12:39 +0300

    start code

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+// import provider
+import { Provider } from "react-redux";
+// import store
+import store from "./reducers";
+
+ReactDOM.render(
+  // The provider will enable the child components to access the store
+  <Provider store={store}>
+	<Router>
+		<App />
+	</Router>
+  </Provider>,
+  document.getElementById("root")
+);
