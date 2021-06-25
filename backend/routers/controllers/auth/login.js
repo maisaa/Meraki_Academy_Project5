7c4fcb84950e8149175b525fcbd1be020713f973
maisaa <maisaa.alkhedr@gmail.com>
2021-06-25 05:43:54 +0200

    login backend

---
+++ backend/routers/controllers/auth/login.js
@@ -0,0 +1,39 @@
+const db = require('./../../../db/db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const login = (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const command = `SELECT * FROM users WHERE email = ?;`;
+        const data = [email, password];
+        db.query(command, data, async (err, result) => {
+            if (err) {
+                res.status(500).json("Internal Server Error");
+            } else if (result.length == 0) {
+                res.status(404).json("This email doesn't exist");
+            } else {
+                const validUser = await bcrypt.compare(password, result[0].password);
+                if (validUser) {
+                    const payload = {
+                        userId: result[0].user_id,
+                        roleId: result[0].role_id,
+                    }
+                    const options = {
+                        expiresIn: '60m',
+                    };
+                    let token = jwt.sign(payload, process.env.SECRET, options);
+                    res.status(200).json(token);
+                } else {
+                    res.status(403).json("The password you've entered is incorrect")
+                }
+            }
+        })
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+module.exports = {
+    login
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db/db');
+const loginRouter = require('./routers/routes/auth/login');
 
 const app = express();
 
 //routers
 
-//built-in middlewares
+//built-in middle wares
 app.use(express.json());
 
 //third-party middleware
 app.use(cors());
 
 //app routers
+app.use(loginRouter)
 
 const PORT = process.env.PORT || 5000;
 
