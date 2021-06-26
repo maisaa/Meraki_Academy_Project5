a434de237931bbba3a6ad4265d2787b2673c1e95
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-26 17:36:19 +0300

    modify addSport FUN

--- backend/routers/controllers/sport.js
+++ backend/routers/controllers/sport.js
@@ -1,7 +1,20 @@
 const connection = require("./../../db/db");
 
-const addSport = (req, res) => {
+const addSport = async (req, res) => {
   const { type, description, photo, video } = req.body;
+  const query = `INSERT INTO sports (type,description,photo,video) VALUES (?,?,?,?) `;
+  const data = [type, description, photo, video];
+  connection.query(query, data, (err, results) => {
+    if (err) return res.status(404).json(err);
+  });
+  const query_select = `select * from sports where type= ? && description = ?`;
+  const data_select = [type, description];
+  const SportAdded = await connection.promise().query(query_select, data_select);
+  res.status(200).json(SportAdded[0]);
+};
+
+const deleteSport = (req, res) => {
+  const sportID = req.params.id;
   const query = `INSERT INTO sports (type,description,photo,video) VALUES (?,?,?,?)`;
   const data = [type, description, photo, video];
   connection.query(query, data, (err, results) => {
