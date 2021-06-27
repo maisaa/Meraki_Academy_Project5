5e55f91781cbf59b607f37baa70cc6db40a84bd2
maisaa <maisaa.alkhedr@gmail.com>
2021-06-27 20:03:27 +0200

    creat login reducer

---
+++ frontend/src/components/auth/login/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
+const Login = () => {
+    const history = useHistory();
+
+}
