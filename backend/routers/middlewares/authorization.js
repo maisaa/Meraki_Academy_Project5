f3afc257963873b66bb88067167135f3b4a71ec6
maisaa <maisaa.alkhedr@gmail.com>
2021-06-27 06:16:22 +0200

    authorization  middlewares

--- backend/routers/middlewares/authorization.js
+++ backend/routers/middlewares/authorization.js
@@ -1,1 +1,23 @@
-const jwt = require('jsonwebtoken');
+const db = require('./../../db/db');
+
+
+const authorization = (string) => {
+    return (req, res, next) => {
+        const { roleId } = req.token.roleId;
+        const command = `SELECT * FROM roles WHERE role_id = ?;`;
+        const data = [roleId];
+        db.query(command, data, async (err, result) => {
+            if (err) {
+                res.status(500).json("Internal Server Error")
+            } else {
+                if (!result.role === string)
+                    return res.status(403).json({
+                        message: 'forbidden'
+                    });
+                next();
+            }
+        })
+    }
+}
+
+module.exports = authorization;
