d9f9d5cfc15247747c7bf3a4c55dcc0d38529014
maisaa <maisaa.alkhedr@gmail.com>
2021-06-28 16:34:44 +0200

    add route login to the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Login from './components/auth/login/index';
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App">App component
+		<Route exact path="/login" component={Login} />
+	</div>;
 };
 
 export default App;
