7c1b8a541aa2297dad3b28ad834fe92ca0c35c37
maisaa <maisaa.alkhedr@gmail.com>
2021-06-28 16:33:58 +0200

    import login in reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 
 //import reducer
+import loginReducer from './login';
 
 
 const reducers = combineReducers({
     // add reducers here
+    loginReducer,
 });
 
 const store = createStore(reducers);
