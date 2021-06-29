15a19de5b2762a551eec629778a40bcbb0a34141
maisaa <maisaa.alkhedr@gmail.com>
2021-06-29 19:47:50 +0200

    sports reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from 'redux';
 
 //import reducer
 import loginReducer from './login';
-
+import sportReducer from './sports';
 
 const reducers = combineReducers({
     // add reducers here
     loginReducer,
+    sportReducer,
+    
 });
 
 const store = createStore(reducers);
