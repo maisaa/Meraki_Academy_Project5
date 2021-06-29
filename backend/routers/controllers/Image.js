8649d984719281a7e5fa6ba46286bf444c61db92
IbrahimBaniSalim <ibrahim.banisalim@gmail.com>
2021-06-29 19:29:03 +0300

    profile front end

--- backend/routers/controllers/Image.js
+++ backend/routers/controllers/Image.js
@@ -1,57 +1,43 @@
 const db = require("./../../db/db");
 
 const getImage = (req, res) => {
-
   const command = `SELECT * FROM images`;
   db.query(command, (err, result) => {
     if (err) return res.status(204);
     res.status(204);
     res.json(result);
   });
+};
+
+const addImage = (req, res) => {
+  const imageObj = {
+    image: req.body.image,
   };
-
-
-
-  const addImage = (req,res) => {
-    const imageObj = {
-        image: req.body.image,
-       
-      };
-    const command = `INSERT INTO images (image)
+  const command = `INSERT INTO images (image)
     VALUES (?);`;
-    const data = [imageObj.image];
-    db.query(command,data, (err, result) => {
-      if (err) throw err;
-       res.json(result);
-    });
-    
- 
-}
+  const data = [imageObj.image];
+  db.query(command, data, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
 
 const deleteImage = (req, res) => {
-    const command = `
+  const command = `
     DELETE FROM images
     WHERE image_id= ? AND is_deleted=0;
     `;
-  
-    const arr = [req.params.id];
-    db.query(command, arr, (err, result) => {
-      if (err)  return res.status(404);
-      res.status(200);
-      res.json(result);
-    });
-  };
 
+  const arr = [req.params.id];
+  db.query(command, arr, (err, result) => {
+    if (err) return res.status(404);
+    res.status(200);
+    res.json(result);
+  });
+};
 
-
-
-
-
-
-
-
-  module.exports = {
-    getImage,
-    addImage,
-    deleteImage
-}
+module.exports = {
+  getImage,
+  addImage,
+  deleteImage,
+};
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,11 +2,12 @@ import { combineReducers, createStore } from 'redux';
 
 //import reducer
 import loginReducer from './login';
-
+import profileReducers from './profile';
 
 const reducers = combineReducers({
     // add reducers here
     loginReducer,
+    profileReducers,
 });
 
 const store = createStore(reducers);
