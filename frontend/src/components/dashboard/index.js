5af95c263e735f71ced4277e85e58f1e8f1746dc
maisaa <maisaa.alkhedr@gmail.com>
2021-07-06 19:10:07 +0200

    add slide show

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -3,8 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSports, getSport } from '../../reducers/sports';
 import { setToken } from '../../reducers/login';
 import { useHistory } from "react-router-dom";
+import {Carousel } from 'react-bootstrap';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import './dashboard.css'
 
 const Dashboard = () => {
 	const [type, setType] = useState("");
@@ -46,19 +48,81 @@ const Dashboard = () => {
 			});
 	}
 
-	const getSportByType =  (e) => {
+	const getSportByType = (e) => {
 		axios.get(`http://localhost:5000/sport/${e.target.value}`)
-		.then((result)=>{
-			setType(e.target.value);
-			dispatch(setSports(result.data));
-			history.push(`/sports`);
-			console.log("result.data....dashboard....",result.data)
-		})
+			.then((result) => {
+				setType(e.target.value);
+				dispatch(setSports(result.data));
+				history.push(`/sports`);
+				console.log("result.data....dashboard....", result.data)
+			})
 	}
 
 	// you could see the state by 
 	console.log('úser', state)
-	return <> {state.loggedIn ?
+	return <>
+		<Carousel fade>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src= 'https://www.davidlloyd.co.uk/cms/images/softwire-david-lloyd/image/upload/f_auto/v1/Tennis/tennis-hero.jpg'
+					alt="Tennis"
+				/>
+				<Carousel.Caption>
+					<h3>Tennis</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src="https://www.tributeboxing.com.au/uploads/slideshow/1536301415qkedwof37eb8x3o8qejg0s80h2s4kd.jpg"
+					alt="Boxing"
+				/>
+				<Carousel.Caption>
+					<h3>Boxing</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src="https://www.runtastic.com/blog/wp-content/uploads/2018/05/thumbnail_1200x800-1-1024x683.jpg"
+					alt="Running"
+				/>
+				<Carousel.Caption>
+					<h3>Running</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src="https://www1.health.gov.au/internet/main/publishing.nsf/AttachmentsByTitle/sport-national-integrity-of-sport-unit-carousel-images/$FILE/10612%20Sport%20images19.jpg"
+					alt="Swimming"
+				/>
+				<Carousel.Caption>
+					<h3>Swimming</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src="https://www.polar.com/sites/default/files/static/categories/team_sports-desktop.jpg"
+					alt="Football"
+				/>
+				<Carousel.Caption>
+					<h3>Football</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100 imgH"
+					src="https://www.polar.com/sites/default/files/static/categories/team_sports-desktop.jpg"
+					alt="Football"
+				/>
+				<Carousel.Caption>
+					<h3>Football</h3>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
 		<div className="App">
 			<h2>Dashboard  for user with Id (  {state.user.userId} )</h2>
 			<h3>All sports</h3>
@@ -70,7 +134,7 @@ const Dashboard = () => {
 					<input type="button" value={elem.type} onClick={getSportByType}></input>
 				</div>
 			))}</div>
-		</div> : <div>You Are Logged out</div>}
+		</div> 
 	</>;
 };
 
