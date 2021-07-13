bd85110716b0e6b351e6ac40b2dbf2a0d545a8b9
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-07-13 19:56:53 +0300

    all member style

--- frontend/src/components/members/members.js
+++ frontend/src/components/members/members.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { setMembers, setMembersType, setUserId } from "../../reducers/members";
 import { decode } from "jsonwebtoken";
+import { Button } from "react-bootstrap";
 
 const Members = () => {
   const dispatch = useDispatch();
@@ -33,58 +34,129 @@ const Members = () => {
   return (
     <>
       {state.id.role == 3 ? (
-        <div className="allmembers">
-          {state.members &&
-            state.members.map((ele) => {
-              console.log("userId", state.id.userId);
-              console.log("posterId", ele.poster_id);
-
-              if (state.id.userId === ele.poster_id) {
-                return (
-                  <div className="">
-                    <button>{ele.firstName}</button>
-                    <button>chat</button>
-                    <button
-                      onClick={async (e) => {
-                        await axios.delete(`http://localhost:5000/usersPost`, {
-                          userId: ele.user_Id,
-                          postId: ele.post_id,
-                        });
-                      }}
-                    >
-                      delete
-                    </button>
+        <div className="allPageMember">
+        <p className="titleHomeMember"> All your subscribers  </p>
+        {state.members.map((ele, i) => {
+          console.log("state.members.length.....",state.members.length);
+          if(state.members.length === 0) {
+            return <div>
+            <p className="titleHomeMember" style={{color:"#2cccc4", marginTop: "1rem"}}> You don't have any subscribers  </p>
+          </div>
+          }
+          if (state.id.userId === ele.poster_id) {
+            return (
+              <div className="allmembers">
+                <div>
+                  {" "}
+                  <img
+                    src={ele.image}
+                    height="150"
+                    width="150"
+                    className="imageMembers"
+                  />
+                </div>
+                <div className="nameMember">
+                  <div className="titleMember">
+                    {" "}
+                    {ele.firstName + " " + ele.lastName}{" "}
                   </div>
-                );
-              }
-            })}
-        </div>
+                  <div>
+                  
+                    <Button
+                      className="styleButtonMember"
+                      variant="outline-dark"
+                    >
+                      Chat
+                    </Button>
+                    <Button
+                      className="styleButtonMember"
+                      onClick={async (e) => {
+                        await axios.delete(
+                          `http://localhost:5000/usersPost`,
+                          {
+                            userId: ele.user_Id,
+                            postId: ele.post_id,
+                          }
+                        );
+                      }}
+                      variant="outline-dark"
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
       ) : state.id.role == 4 ? (
-        <div className="allmembers">
+        <div className="allPageMember">
+          <p className="titleHomeMember"> All your subscribers  </p>
           {state.members.map((ele) => {
-            if (state.id.userId === ele.poster_id) {
+            console.log(".........state.members.length.....",state.members.length);
+            if(state.members.length === 0) {
+              return <div>
+              <p className="titleHomeMember" style={{color:"#2cccc4", marginTop: "1rem"}}> You don't have any subscribers  </p>
+            </div>
+            }
+            else if (state.id.userId === ele.poster_id) {
               return (
-                <div className="">
-                  <button>{ele.firstName}</button>
-                  <button>video</button>
-                  <button>chat</button>
-                  <button
-                    onClick={async (e) => {
-                      await axios.delete(`http://localhost:5000/usersPost`, {
-                        userId: ele.user_Id,
-                        postId: ele.post_id,
-                      });
-                    }}
-                  >
-                    delete
-                  </button>{" "}
+                <div className="allmembers">
+                  <div>
+                    {" "}
+                    <img
+                      src={ele.image}
+                      height="150"
+                      width="150"
+                      className="imageMembers"
+                    />
+                  </div>
+                  <div className="nameMember">
+                    <div className="titleMember">
+                      {" "}
+                      {ele.firstName + " " + ele.lastName}{" "}
+                    </div>
+                    <div>
+                      <Button
+                        className="styleButtonMember"
+                        variant="outline-dark"
+                      >
+                        Video
+                      </Button>
+                      <Button
+                        className="styleButtonMember"
+                        variant="outline-dark"
+                      >
+                        Chat
+                      </Button>
+                      <Button
+                        className="styleButtonMember"
+                        onClick={async (e) => {
+                          await axios.delete(
+                            `http://localhost:5000/usersPost`,
+                            {
+                              userId: ele.user_Id,
+                              postId: ele.post_id,
+                            }
+                          );
+                        }}
+                        variant="outline-dark"
+                      >
+                        Delete
+                      </Button>
+                    </div>
+                  </div>
                 </div>
               );
             }
           })}
         </div>
       ) : (
-        <div></div>
+        <div>
+          <p className="titleHomeMember"> All your subscribers  </p>
+          <p className="titleHomeMember" style={{color:"#2cccc4", marginTop: "1rem"}}> You don't have any subscribers  </p>
+        </div>
       )}
     </>
   );
